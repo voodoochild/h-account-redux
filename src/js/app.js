c2b2ebeafdf35c0dac5e46cd28602b60523db003
Kriss Watt <kriss.watt@gmail.com>
2015-08-11 16:34:08 +0100

    Added bookings and find-booking pages

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+(function () {
+    let navToggle = document.getElementById('nav-toggle');
+    let siteNav = document.getElementById('account-nav');
+    let userToggle = document.getElementById('user-toggle');
+
+    function signout () {
+        document.body.classList.remove('signed-in');
+        document.body.classList.add('signed-out');
+    }
+
+    function signin () {
+        document.body.classList.add('signed-in');
+        document.body.classList.remove('signed-out');
+    }
+
+    userToggle.addEventListener('click', () => {
+        if (document.body.classList.contains('signed-out')) { signin(); } else  { signout(); }
+    });
+
+    navToggle.addEventListener('click', () => siteNav.classList.toggle('expanded'));
+})();
