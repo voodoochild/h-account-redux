2fd53a404f0a0a2cdb631b934fa43b4d9aeb30f1
Kriss Watt <kriss.watt@gmail.com>
2015-08-12 10:14:13 +0100

    Added Browsersync, dummy content pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,20 @@ var sass = require('gulp-sass');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 function clean () {
-    del.sync(['./dist'], { force: true }, function (err, paths) {
+    del.sync(['./dist/*'], { force: true }, function (err, paths) {
         if (err) { return console.error(err); }
         console.log('Deleted files/folders:\n', paths.join('\n'));
     });
 }
 
 function moveStaticFiles () {
-    gulp.src('./src/**/*.html').pipe(gulp.dest('./dist'));
-    gulp.src('./src/images/**/*').pipe(gulp.dest('./dist/images'));
-    gulp.src('./src/fonts/**/*').pipe(gulp.dest('./dist/fonts'));
+    gulp.src('./src/**/*.html').pipe(gulp.dest('./dist')).pipe(reload({ stream: true }));
+    gulp.src('./src/images/**/*').pipe(gulp.dest('./dist/images')).pipe(reload({ stream: true }));
+    gulp.src('./src/fonts/**/*').pipe(gulp.dest('./dist/fonts')).pipe(reload({ stream: true }));
 }
 
 function buildCSS () {
@@ -22,7 +24,8 @@ function buildCSS () {
         .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/styles'));
+        .pipe(gulp.dest('./dist/styles'))
+        .pipe(reload({ stream: true }));
 }
 
 function buildJS () {
@@ -30,7 +33,8 @@ function buildJS () {
         .pipe(babel())
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/js'));
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(reload({ stream: true }));
 }
 
 gulp.task('build', function () {
@@ -40,11 +44,17 @@ gulp.task('build', function () {
     buildJS();
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/**/*.html', moveStaticFiles);
+gulp.task('serve', ['build'], function () {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    gulp.watch(['src/**/*.html', 'src/images/**/*', 'src/fonts/**/*'], moveStaticFiles);
     gulp.watch('src/styles/**/*.scss', buildCSS);
     gulp.watch('src/js/**/*.js', buildJS);
 });
 
 gulp.task('clean', function () { clean(); });
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['serve']);
